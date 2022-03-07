3f2b94e9bb91b1a56820bf62772fb545bff7b6d9
Koji Abe <gracehime@gmail.com>
2022-03-07 12:47:23 +0900

    p42 3.5 LedDriver_Createの引数のポインタ変数をstatic変数に保存するコードを追加。LedDriver_TurnOnでstatic変数に1を書き込むシンプルなやり方でテストを成功させる。

--- tests/LedDriver/LedDriverTest.cpp
+++ tests/LedDriver/LedDriverTest.cpp
@@ -16,12 +16,7 @@ TEST_GROUP(LedDriver)
     }
 };
 
-TEST(LedDriver, LedsOffAfterCreate)
-{
-  uint16_t virtualLeds = 0xffff;
-  LedDriver_Create(&virtualLeds);
-  LONGS_EQUAL(0, virtualLeds);
-}
+// テストは下に書いてあるものから先に実行される
 
 TEST(LedDriver, TurnOnLedOne)
 {
@@ -30,3 +25,11 @@ TEST(LedDriver, TurnOnLedOne)
   LedDriver_TurnOn(1);
   LONGS_EQUAL(1, virtualLeds);
 }
+
+TEST(LedDriver, LedsOffAfterCreate)
+{
+  uint16_t virtualLeds = 0xffff;
+  LedDriver_Create(&virtualLeds);
+  LONGS_EQUAL(0, virtualLeds);
+}
+
