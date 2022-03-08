500acdd94e7fbba561b16cb6017a64501ac77ad6
Koji Abe <gracehime@gmail.com>
2022-03-09 07:27:57 +0900

    p66 LedDriver_TurnOffのガード節を削除し、OutOfBoundsTurnOffDoesNoHarmテストの成功を確認した。前のコミットでテストを追加したが手順を間違えた。LedDriver_TurnOffのガード節を削除→OutOfBoundsTurnOffDoesNoHarmテストを追加→テストの失敗を確認、が正しい手順だった。

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -37,9 +37,6 @@ void LedDriver_TurnOn(int ledNumber)
 
 void LedDriver_TurnOff(int ledNumber)
 {
-    if (ledNumber <= 0 || ledNumber > 16)
-        return;
-
     ledsImage &= ~(convertLedNumberToBit(ledNumber));
     updateHardware();
 }
