c71683b50bfae05418401306aeaca34112b0158b
Koji Abe <gracehime@gmail.com>
2022-03-08 23:57:18 +0900

    p63 境界値と大きく範囲を外れた値を与えるテストOutOfBoundsChangesNothingを追加した。テストの失敗を確認した。

--- tests/LedDriver/LedDriverTest.cpp
+++ tests/LedDriver/LedDriverTest.cpp
@@ -74,3 +74,12 @@ TEST(LedDriver, UpperAndLowerBounds)
   LedDriver_TurnOn(16);
   LONGS_EQUAL(0x8001, virtualLeds);
 }
+
+TEST(LedDriver, OutOfBoundsChangesNothing)
+{
+  LedDriver_TurnOn(-1);
+  LedDriver_TurnOn(0);
+  LedDriver_TurnOn(17);
+  LedDriver_TurnOn(3141);
+  LONGS_EQUAL(0, virtualLeds);
+}
